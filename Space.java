e026b226827b1def9d9f921090b9e84b7387d91f
ZachRocco <60672549+ZachRocco@users.noreply.github.com>
2020-02-06 13:35:26 -0800

    Lab 1
    The Distinguished version of lab 1 of the asteroids project

--- Space.java
+++ Space.java
@@ -10,6 +10,7 @@ public class Space extends World
 {
     private Counter scoreCounter;
     private int startAsteroids = 3;
+    private GreenfootImage background;
 
     /**
      * Create the space and all objects within it.
@@ -17,7 +18,7 @@ public class Space extends World
     public Space() 
     {
         super(600, 500, 1);
-        GreenfootImage background = getBackground();
+        background = getBackground();
         background.setColor(Color.BLACK);
         background.fill();
         
@@ -25,6 +26,7 @@ public class Space extends World
         addObject(rocket, getWidth()/2 + 100, getHeight()/2);
         
         addAsteroids(startAsteroids);
+        paintStars(300);
         
         scoreCounter = new Counter("Score: ");
         addObject(scoreCounter, 60, 480);
@@ -44,6 +46,23 @@ public class Space extends World
             int x = Greenfoot.getRandomNumber(getWidth()/2);
             int y = Greenfoot.getRandomNumber(getHeight()/2);
             addObject(new Asteroid(), x, y);
+        }
+    }
+    
+    private void paintStars(int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = Greenfoot.getRandomNumber(getHeight());
+            int size = Greenfoot.getRandomNumber(1);
+            int tintRed = Greenfoot.getRandomNumber(25);
+            int tintBlue = Greenfoot.getRandomNumber(25);
+            int tintGreen = Greenfoot.getRandomNumber(25);
+            int brightness = Greenfoot.getRandomNumber(230);
+            background.setColor(new Color(brightness + tintRed,brightness + tintBlue,
+            brightness + tintGreen));
+            background.fillOval(x, y, 2 + size, 2 + size);
         }
     }
     
